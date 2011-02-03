ea02cff4027be5443407d8e714904387c2fee738
chertov <maxim@chertov.me>
2011-02-03 15:46:49 +0300

    rm test file

--- test/test_one_only.py
+++
@@ -1,8 +0,0 @@
-import os
-from functools import partial
-
-command = '.\\..\\run.py --verbose=0 --output=vs7 Qt_OpenGL_without_warning\\GLThread.h'
-
-#print command
-os.system(command)
-os.system('pause')
